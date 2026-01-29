14c546a92389d6ccfa0fd8fb58ed6e10a3d1d530
maxschoolgit <maxw76@nycstudents.net>
2026-01-29 11:19:39 -0500

    six seven

--- besttexteditor.py
+++ besttexteditor.py
@@ -1,18 +1,27 @@
 from tkinter import *
 from tkmacosx import Button
 
+def writeFile():
+	with open("demofile.txt", "w") as f:
+		f.write(typing_box.get("1.0",END))
+
+def readFile():
+	with open("demofile.txt", '')
+
 #Creates window
 root = Tk()
 root.title("This Is The Greatest Text Editor Of All Time")
 root.geometry("600x400")
 
 #Buttons, text, etc.
-file_button = Button(root, text="File")
+file_button = Button(root, text="Save", command=writeFile)
+fileload_button = Button(root, text="Load", command=readFile)
 typing_box = Text(root, bg = 'light grey', fg = 'black', bd = 5, font = 'Times')
 
 
 #Pack/Grid
 file_button.pack()
+fileload_button.pack()
 typing_box.pack()
 
 #GUI Event Loop
--- devtesting.py
+++ devtesting.py
@@ -1,7 +1,10 @@
 from tkinter import *
 from tkmacosx import Button
+import random
 
 #Heavy W.I.P.
+
+
 
 #Creates window
 root = Tk()
@@ -9,15 +12,42 @@ root.title("Updating Labels")
 root.geometry("600x400")
 
 #define function
-def changeTitle():
-	root.title(T.get("1.0",END))
+def ButtonUpdate1():
+	one_label.config(text = "Updated 1")
+
+
+def ButtonUpdate2():
+	two_label.config(text = "Updated 2")
+
+
+def ButtonUpdate3():
+    three_label.config(text = "Updated 3")
+
+def RandomButton():
+	r = random.randint(1, 3)
+	if r == 1:
+		one_button.config(text = 'Randomly Picked')
+	if r == 2:
+		two_button.config(text = 'Randomly Picked')
+	if r == 3:
+		three_button.config(text = 'Randomly Picked')
 
 #Create button
-red_button = Button(root, text='change title!', command=changeTitle)
-T = Text(root, height = 5, width = 32)
+one_button = Button(root, text='Update 1', command=ButtonUpdate1)
+two_button = Button(root, text='Update 2', command=ButtonUpdate2)
+three_button = Button(root, text='Update 3', command=ButtonUpdate3)
+random_button = Button(root, text='Random Pick', command=RandomButton)
+one_label = Label(root, text='Label 1')
+two_label = Label(root, text='Label 2')
+three_label = Label(root, text='Label 3')
 
 #pack
-red_button.pack()
-T.pack()
+one_button.pack()
+two_button.pack()
+three_button.pack()
+one_label.pack()
+two_label.pack()
+three_label.pack()
+random_button.pack()
 
 root.mainloop()
